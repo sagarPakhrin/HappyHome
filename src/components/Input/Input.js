daa783b01d13c4e570a469f58aa8f958f536b290
sagarpakhrin <sagarlama826@gmail.com>
2021-07-17 23:18:44 +0545

    style(Input): fix input styling

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -4,23 +4,25 @@ import React from 'react';
 const Input = ({ label, ...props }) => {
   const [field, meta] = useField(props);
   return (
-    <div className="flex flex-col text-left relative">
-      {label && (
-        <label
-          htmlFor={props.id || props.name}
-          className={`text-sm transition-all transition-500 absolute text-coolGray-400 ${
-            field.value ? '-top-3 ' : 'top-1 font-medium'
-          }`}
-        >
-          {label}
-        </label>
-      )}
-      <input
-        {...field}
-        {...props}
-        className="py-1 border-b border-gray-300 focus:outline-none focus:border-blue-500"
-        autoComplete="off"
-      />
+    <div className="flex flex-col text-left">
+      <div className="relative">
+        {label && (
+          <label
+            htmlFor={props.id || props.name}
+            className={`text-sm transition-all transition-500 absolute text-coolGray-400 ${
+              field.value ? '-top-3 ' : 'top-1 font-medium'
+            }`}
+          >
+            {label}
+          </label>
+        )}
+        <input
+          {...field}
+          {...props}
+          className="w-full py-1 border-b border-gray-300 focus:outline-none focus:border-blue-500"
+          autoComplete="off"
+        />
+      </div>
       {meta.touched && meta.error ? (
         <div className="text-red-500 text-sm">{meta.error}</div>
       ) : null}
